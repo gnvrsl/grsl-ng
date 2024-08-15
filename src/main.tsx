6c28dcce106479af63ce392ac0f7da790d4bf65a
Chris Ribe <chrisribe@gmail.com>
2024-08-15 14:38:39 +0000

    add logos and landing page template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import LandingPage from './LandingPage.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LandingPage />
   </StrictMode>,
 )
